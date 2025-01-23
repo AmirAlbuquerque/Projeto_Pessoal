2b66d52a310dec443fbc7103216eb272daaa2c99
Amir Albuquerque <amirsilva00an@gmail.com>
2025-01-23 19:18:56 -0300

    Remoção da leitura dinâmica

--- personal_project.js
+++ personal_project.js
@@ -3,20 +3,6 @@ let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let contador = 1;
 
-//Função para utilizar a voz
-function exibirTexto(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-    if ('speechSynthesis' in window) {
-        let utterance = new SpeechSynthesisUtterance(texto);
-        utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.3; 
-        window.speechSynthesis.speak(utterance); 
-    } else {
-        console.log("Web Speech API não suportada neste navegador.");
-    }
-}
-
 //Função que define a mensagem inicial
 function mensagemInicial(){
     exibirTexto("h1", "Jogo do Número Secreto");
